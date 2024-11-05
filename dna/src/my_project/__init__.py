17d8a3b5cec95d2c51a5cd864fdbae79ca8c674e
cpaul <caina_rose.paul@dfki.de>
2024-11-05 18:40:44 +0100

    Eval with noise

---
+++ dna/src/my_project/__init__.py
@@ -0,0 +1,2 @@
+# src/my_project/__init__.py
+from .pipelines import dna_analysis  # Makes dna_analysis accessible from my_project
---
+++ dna/src/my_project/pipelines/__init__.py
@@ -0,0 +1,2 @@
+# src/my_project/pipelines/__init__.py
+from .dna_analysis import load_data, train_model  # Direct access to functions from dna_analysis
